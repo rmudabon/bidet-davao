c4f168b55af002ed8d7d3ccad9cf4cd31fe2a15a
Your Name <you@example.com>
2025-12-07 09:45:08 +0800

    feat: first commit

---
+++ bidet_davao/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bidet_davao project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bidet_davao.settings')
+
+application = get_asgi_application()
