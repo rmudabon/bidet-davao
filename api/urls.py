ad69c308f71b25a69cfc4646b6790f53915d37dd
rmudabon <rmdabs@gmail.com>
2025-12-30 22:34:41 +0800

    feat: add auth endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from .views import LocationViewSet
+from .views import LocationViewSet, LoginView, LogoutView
 
 router = routers.DefaultRouter()
 router.register(r"locations", LocationViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,43 @@
+from django.contrib.auth import authenticate, logout, login
+
 from locations.models import Location
 from locations.serializers import LocationSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.views import APIView
 
 # Create your views here.
 class LocationViewSet(viewsets.ModelViewSet):
     """
     API endpoint to view locations
     """
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     queryset = Location.objects.all().order_by("-created_at")
     serializer_class = LocationSerializer
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({"detail": "Login successful."}, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": "Credentials are invalid."}, status=status.HTTP_400_BAD_REQUEST)
+        
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response({"detail": "User is not logged in."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        logout(request)
+        return Response({"detail": "Logout successful."}, status=status.HTTP_200_OK)
