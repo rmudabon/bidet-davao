9eca929614d6a42d539101aeada86ea9a8397769
rmudabon <rmdabs@gmail.com>
2026-04-12 17:11:05 +0800

    fix: add rest_framework installed apps

--- bidet_davao/settings/base.py
+++ bidet_davao/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'rest_framework',
     'api',
     'locations',
     'drf_spectacular',
