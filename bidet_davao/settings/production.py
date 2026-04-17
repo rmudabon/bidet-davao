46cfc21f85fefa201e26168d8132a0e842c138a9
rmudabon <rmdabs@gmail.com>
2026-04-17 17:19:36 +0800

    fix: env var

--- bidet_davao/settings/production.py
+++ bidet_davao/settings/production.py
@@ -8,7 +8,7 @@ USE_S3 = env.bool('USE_S3', default=False)
 
 if USE_S3:
     AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default=None)
-    AWS_STORAGE_BUCKET_NAME = env('AWS_S3_BUCKET_NAME', default=None)
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME', default=None)
 
     # S3 Settings
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
