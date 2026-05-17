8d16b757fabec71cb5d053137e802a773e6e6d0b
rmudabon <rmdabs@gmail.com>
2026-05-17 22:58:37 +0800

    feat: add reverse geocode

--- geolocation/urls.py
+++ geolocation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from geolocation.views import GeoAutoCompleteView
+from geolocation.views import GeoAutoCompleteView, GeoReverseGeoCodingView
 
 urlpatterns = [
     path('autocomplete/', GeoAutoCompleteView.as_view(), name='autocomplete'),
+    path('reverse/', GeoReverseGeoCodingView.as_view(), name='reverse-geocoding'),
 ]
--- geolocation/views.py
+++ geolocation/views.py
@@ -16,6 +16,46 @@ class GeoAutoCompleteView(APIView):
         
         try:
             results = self.client.pelias_autocomplete(search_text, country='PH', layers=['address', 'venue'])
+            features = results.get('features', [])
+            first_five_suggestions = features[:5] if features else []
+            parsed_suggestions = []
+            for suggestion in first_five_suggestions:
+                properties = suggestion.get('properties', {})
+                geometry = suggestion.get('geometry', {})
+                coordinates = geometry.get('coordinates', [None, None])
+                parsed_suggestions.append({
+                    'label': properties.get('label'),
+                    'street': properties.get('street'),
+                    'name': properties.get('name'),
+                    'longitude': coordinates[0], # ORS returns [long, lat]
+                    'latitude': coordinates[1]
+                })
+
+            return Response(parsed_suggestions)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class GeoReverseGeoCodingView(APIView):
+    client = openrouteservice.Client(key=settings.ORS_API_KEY)
+
+    def get(self, request):
+        latitude = request.query_params.get('latitude')
+        longitude = request.query_params.get('longitude')
+        if not latitude or not longitude:
+            return Response({"error": "Missing 'latitude' or 'longitude' query parameter."}, status=status.HTTP_400_BAD_REQUEST)
+        point = [float(longitude), float(latitude)]  # ORS expects [lon, lat]
+        
+        try:
+            results = self.client.pelias_reverse(point, country='PH', layers=['address', 'venue'], size=1)
+            features = results.get('features', [])
+            if features:
+                properties = features[0].get('properties', {})
+                parsed_features = {
+                    'label': properties.get('label'),
+                    'street': properties.get('street'),
+                }
+
+                return Response(parsed_features)
             return Response(results)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
